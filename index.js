d31f18fb1694dbd9071d36825e7114dba5681d47
Michael Pope <popemray1986@gmail.com>
2018-07-03 02:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const app = "I don't do much.";
 const token = 'ddb1be0288a77fd6ff858b2cbea8fa0e155cad18'
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
-
-
-ddb1be0288a77fd6ff858b2cbea8fa0e155cad18
+fetch('https://api.github.com/user/repos'{
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
